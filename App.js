9b709168ba17867f20a2bcf1adc1935c0b968469
MillanUka <ukamillan@gmail.com>
2020-11-19 11:02:42 +1300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Header, Divider } from "react-native-elements";
 
+function test() {
+  console.log("test");
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Header
+        backgroundColor={""}
+        leftComponent={<Button icon={"menu"} color={"#fff"} />}
+        centerComponent={{ text: "Diet Tracker", style: { color: "#fff" } }}
+        rightComponent={<Button icon={"home"} color={"#fff"} onPress={() => {
+          console.log("home");
+        }} />}
+      />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Divider />
+        <Button title="test"></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    textAlign: "justify",
+    padding : "1%",
+    fontFamily : "Helvetica"
+  }
 });
